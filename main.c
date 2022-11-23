949b59d4d2dc996806b248cea0608aa8ecb1b2a2
kensley24 <kensleyschonauer@gmail.com>
2022-11-23 12:37:12 -0500

    hello

--- main.c
+++ main.c
@@ -4,43 +4,43 @@ int main(int argc, char *argv[])
 {
     // Initialize a random seed for the random number generators
     srand(time(NULL));
-
+    // kfewijfs[ofjs[e9fes]]
     // You may change this code; this is for demonstration purposes
     BuildingType building;
     initBuilding(&building);
     populateRooms(&building);
-    
+
     //Initializing all the hunters
     char name[MAX_STR];
     for (int i = 0; i < MAX_HUNTERS; i++){
     	printf("Enter the name of the hunter: ");
 	fgets(name, MAX_STR, stdin);
-	
+
 	HunterType* newHunter = malloc(sizeof(HunterType));
 	EvidenceClassType evidence = i+1;
-	
+
 	//testing
 	//RoomNodeType* head = building.rooms.head->room->rooms->head;
 	//if (head == NULL) printf("NULL\n");
 	//printf("%s", head->room->name);
-	
+
 	initHunter(&newHunter, building.rooms.head, evidence, name);
 	addHunter(&building.hunters, newHunter);
     }
-    
+
     for (int i = 0; i < MAX_HUNTERS; i++){
     	printf("%s", building.hunters.hunters[i]->name);
     }
-    
-    
-    
+
+
+
     //Initializing the ghost
     GhostType ghost;
-    
+
     //getting ghosttype
     int ghostClassNumber = randInt(1, 4);
     GhostClassType ghostClass = ghostClassNumber;
-    
+
     //getting random room
     int roomNumber = randInt(1, 12); //13 is the amount of rooms given so move 12 times to get there
     RoomNodeType* currRoom = building.rooms.head;
@@ -48,14 +48,14 @@ int main(int argc, char *argv[])
     	currRoom = currRoom->next;
     }
     ghost.room = currRoom->room; //ghost has its room
-    
+
     currRoom->room->ghost = &ghost; //room has the ghost
-    
+
     building.ghost = &ghost; //building has the ghost
-   
-   
-   
-   
+
+
+
+
     cleanupBuilding(&building);
     return 0;
 }
@@ -63,10 +63,10 @@ int main(int argc, char *argv[])
 
 /*
   Function:  randInt
-  Purpose:   returns a pseudo randomly generated number, 
+  Purpose:   returns a pseudo randomly generated number,
              in the range min to (max - 1), inclusively
        in:   upper end of the range of the generated number
-   return:   randomly generated integer in the range [min, max-1) 
+   return:   randomly generated integer in the range [min, max-1)
 */
 int randInt(int min, int max)
 {
@@ -75,10 +75,10 @@ int randInt(int min, int max)
 
 /*
   Function:  randFloat
-  Purpose:   returns a pseudo randomly generated number, 
+  Purpose:   returns a pseudo randomly generated number,
              in the range min to max, inclusively
        in:   upper end of the range of the generated number
-   return:   randomly generated integer in the range [0, max-1) 
+   return:   randomly generated integer in the range [0, max-1)
 */
 float randFloat(float a, float b) {
     // Get a percentage between rand() and the maximum
